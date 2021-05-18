f60150b78baadeaf39401bfa2cd6d2771d34e814
ibezgin <kurt1v@yandex.ru>
2021-05-18 21:24:46 +0300

    fix ping service

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import { config } from "./config";
 import { Logger } from "./logger";
 import { server } from "./server";
+import hsp from "heroku-self-ping";
 
 const startServer = async () => {
     const app = await server();
+    const isHsp = hsp(
+        process.env.HEROKU_APP_NAME
+            ? process.env.HEROKU_APP_NAME
+            : "skapa-promocode.herokuapp.com",
+        { verbose: true, interval: 20000 },
+    );
+
+    Logger.info(`hsp: ${isHsp}`);
 
     app.listen(config.port, () => {
         Logger.info(`
