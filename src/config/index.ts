4e6ab47bb2217a3d9097835577ae71fa64c8e43f
bezgin.i <kurt1v@yandex.ru>
2021-06-18 22:30:50 +0300

    add token

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,13 +14,7 @@ export const config = {
     endpointPrefix: process.env.ENDPOINT_PREFIX || "api",
     secret:
         process.env.SECRET || "WxW0NZBes2BO0RizsjUgj2snooPj3G8KDJm5gTD04gy1wsw",
-    backoffice_token:
-        process.env.BACKOFFICE_TOKEN ||
-        "WxW0NZBes2BO0RizsjUgj2snooPj3G8KDJm5gTD04gy1wsw",
-    game_token:
-        process.env.GAME_TOKEN ||
-        "WxW0NZBes2BO0RizsjUgj2snooPj3G8KDJm5gTD04gy1wsw",
-    site_token:
-        process.env.SITE_TOKEN ||
-        "WxW0NZBes2BO0RizsjUgj2snooPj3G8KDJm5gTD04gy1wsw",
+    backoffice_token: process.env.BACKOFFICE_TOKEN || "backoffice",
+    game_token: process.env.GAME_TOKEN || "game",
+    site_token: process.env.SITE_TOKEN || "site",
 };
